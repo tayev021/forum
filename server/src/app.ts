2ec1fc019e7cad85228d7e43b8722a8a99dc39c7
Eugene Taranov <tayev021@gmail.com>
2025-11-02 22:04:57 +0200

    feat(server): update app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import { rootRouter } from './routes/rootRouter';
+import { globalErrorHandler } from './controllers/errorController';
 
 const app = express();
 
-app.use(rootRouter);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1/', rootRouter);
+
+app.use(globalErrorHandler);
 
 export { app };
