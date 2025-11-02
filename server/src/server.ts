12e3cafa928019287f40bb861d5451a09927d5cb
Eugene Taranov <tayev021@gmail.com>
2025-11-02 22:05:27 +0200

    feat(server): update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,13 @@
 import { app } from './app';
+import { database } from './config/database';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Server is listening'));
+database
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => console.log('Server is listening'));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
