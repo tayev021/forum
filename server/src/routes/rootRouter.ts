bf917e1bee316d2568146eb86cee816561c01f93
Eugene Taranov <tayev021@gmail.com>
2025-11-03 21:56:35 +0200

    feat(server): update rootRouter

--- server/src/routes/rootRouter.ts
+++ server/src/routes/rootRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authRouter } from './authRouter';
+import { postRouter } from './postRouter';
 import { notFound } from '../controllers/notFoundController';
 
 const rootRouter = Router();
 
 rootRouter.use('/auth', authRouter);
+rootRouter.use('/posts', postRouter);
 rootRouter.use(notFound);
 
 export { rootRouter };
