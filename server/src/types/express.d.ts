b6b31f270ed11c9dfce697e2d120ab4255d2be95
Eugene Taranov <tayev021@gmail.com>
2025-11-03 21:47:20 +0200

    feat(server): add express types

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import express from 'express';
+import { User } from '../models';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
