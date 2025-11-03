5fb69fb3469c632abff08db131a0c4f5b859fdf1
Eugene Taranov <tayev021@gmail.com>
2025-11-03 21:53:04 +0200

    feat(server): add protect middleware

---
+++ server/src/middleware/protect.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { AppError } from '../utils/AppError';
+import { verifyJwtAsync } from '../utils/verifyJwtAsync';
+import { User } from '../models';
+
+export async function protect(req: Request, res: Response, next: NextFunction) {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new AppError(
+      401,
+      'You are not signed in! Please sign in to get access.'
+    );
+  }
+
+  const decoded = await verifyJwtAsync(token);
+  const currentUser = await User.findByPk(decoded.id);
+
+  if (!currentUser) {
+    throw new AppError(401, 'This token belongs to a user who does not exist.');
+  }
+
+  currentUser.dataValues.password = '';
+  req.user = currentUser.dataValues;
+  next();
+}
