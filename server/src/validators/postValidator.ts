bbf13bfad49469b6fd9aedadd3bd14e02661f893
Eugene Taranov <tayev021@gmail.com>
2025-11-04 10:01:28 +0200

    fix(server): fix typo

--- server/src/validators/postValidator.ts
+++ server/src/validators/postValidator.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const postSchema = z.object({
   content: z
     .string()
-    .max(1024, 'Password must be a maximum of 1024 characters.'),
+    .max(1024, 'Post content must be a maximum of 1024 characters.'),
 });
