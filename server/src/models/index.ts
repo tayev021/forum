5c1ddd42e4c0d6ec906780c9c5fdbc5f9d32d8e6
Eugene Taranov <tayev021@gmail.com>
2025-11-04 21:04:21 +0200

    feat(server): update database associations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,8 +2,15 @@ import { database } from '../config/database';
 import { ErrorLog } from './ErrorLogModel';
 import { Post } from './PostModel';
 import { User } from './UserModel';
+import { Thread } from './ThreadModel';
 
 User.hasMany(Post, { foreignKey: 'authorId' });
 Post.belongsTo(User, { foreignKey: 'authorId' });
 
-export { database, ErrorLog, User, Post };
+User.hasMany(Thread, { foreignKey: 'authorId' });
+Thread.belongsTo(User, { foreignKey: 'authorId' });
+
+Thread.hasMany(Post, { foreignKey: 'threadId' });
+Post.belongsTo(Thread, { foreignKey: 'threadId' });
+
+export { database, ErrorLog, User, Thread, Post };
