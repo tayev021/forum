1a32edca80b3e408334a2ee07c7a9cde3d725216
Eugene Taranov <tayev021@gmail.com>
2025-11-04 09:54:52 +0200

    fix(server): rename createPostValidator to postValidator

--- server/src/routes/postRouter.ts
+++ server/src/routes/postRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { protect } from '../middleware/protect';
 import { validate } from '../middleware/validate';
-import { createPostSchema } from '../validators/createPostValidator';
+import { postSchema } from '../validators/postValidator';
 import { createPost } from '../controllers/postController';
 
 const postRouter = Router();
 
-postRouter.post('/', protect, validate(createPostSchema), createPost);
+postRouter.post('/', protect, validate(postSchema), createPost);
 
 export { postRouter };
--- server/src/validators/postValidator.ts
+++ server/src/validators/postValidator.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const createPostSchema = z.object({
+export const postSchema = z.object({
   content: z
     .string()
     .max(1024, 'Password must be a maximum of 1024 characters.'),
