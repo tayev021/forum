3e677fc6374d92384b05818eec77e90c8e0d21ad
Eugene Taranov <tayev021@gmail.com>
2025-11-04 21:01:19 +0200

    feat(server): update validate middleware

--- server/src/middleware/validate.ts
+++ server/src/middleware/validate.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodType } from 'zod';
 
-export function validate<T>(schema: ZodType<T>) {
+export function validate(...schemas: ZodType<any>[]) {
   return (req: Request, res: Response, next: NextFunction) => {
-    const result = schema.safeParse(req.body);
+    for (const schema of schemas) {
+      const result = schema.safeParse(req.body);
 
-    if (!result.success) {
-      const errors = result.error.issues.map((e) => ({
-        field: e.path.join('.'),
-        message: e.message,
-      }));
+      if (!result.success) {
+        const errors = result.error.issues.map((e) => ({
+          field: e.path.join('.'),
+          message: e.message,
+        }));
 
-      return res.status(400).json({ errors });
+        return res.status(400).json({ errors });
+      }
     }
 
     return next();
