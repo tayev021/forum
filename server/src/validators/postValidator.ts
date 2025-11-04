b5a64c17c6b498a4dd495f389ec3769f0cdc27ad
Eugene Taranov <tayev021@gmail.com>
2025-11-04 20:59:29 +0200

    fix(server): fix post validation schema

--- server/src/validators/postValidator.ts
+++ server/src/validators/postValidator.ts
@@ -3,5 +3,6 @@ import { z } from 'zod';
 export const postSchema = z.object({
   content: z
     .string()
+    .min(1, 'Post content must be at least one character long.')
     .max(1024, 'Post content must be a maximum of 1024 characters.'),
 });
