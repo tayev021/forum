727f26509e26454d83f4d2125d6dff9a1363f42b
Eugene Taranov <tayev021@gmail.com>
2025-11-07 22:13:57 +0200

    feat(client): update Providers

--- client/src/app/providers/index.tsx
+++ client/src/app/providers/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from 'react';
 import { StoreProvider } from './StoreProvider';
 import { ThemeProvider } from './ThemeProvider';
+import { ModalProvider } from './ModalProvider';
 
 interface ProvidersProps {
   children: ReactElement[];
@@ -9,7 +10,9 @@ interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   return (
     <StoreProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </ThemeProvider>
     </StoreProvider>
   );
 }
