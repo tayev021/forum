e0c814d9afa24306c8e5950dd5f5e2ed39f8e123
Eugene Taranov <tayev021@gmail.com>
2025-11-07 19:22:24 +0200

    feat(client): add shared ThemeToggler ui

---
+++ client/src/shared/ui/ThemeToggler.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import type { Theme } from '../lib/types/Theme';
+import { useTheme } from '../lib/hooks/useTheme';
+import { HiMoon, HiSun } from 'react-icons/hi2';
+
+interface ButtonProps {
+  $theme: Theme;
+}
+
+const Button = styled.button<ButtonProps>`
+  display: flex;
+  align-items: center;
+  color: var(--color-emerald-900);
+  transition: all linear 0.1s;
+
+  &:hover {
+    color: var(--color-emerald-950);
+    transform: scale(1.1);
+  }
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  cursor: pointer;
+`;
+
+export function ThemeToggler() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button $theme={theme} onClick={toggleTheme}>
+      {theme === 'light' ? <HiMoon /> : <HiSun />}
+    </Button>
+  );
+}
