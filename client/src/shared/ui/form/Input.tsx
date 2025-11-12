743ec7efa93df8e370992cb1fa663552b337147d
Eugene Taranov <tayev021@gmail.com>
2025-11-12 19:33:14 +0200

    feat(client): add shared Form.Input component

---
+++ client/src/shared/ui/form/Input.tsx
@@ -0,0 +1,43 @@
+import { type ChangeEvent, type FocusEvent } from 'react';
+import styled from 'styled-components';
+
+interface InputProps {
+  id: string;
+  type: 'text' | 'password';
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
+}
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 0.8rem 1rem;
+  border: 2px solid var(--color-primary);
+  border-radius: 0.8rem;
+  font-size: 1.6rem;
+  line-height: 1;
+  background-color: var(--color-bg-secondary);
+  box-shadow: var(--shadow-small);
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export function Input({
+  id,
+  type = 'text',
+  value,
+  onChange,
+  onBlur,
+}: InputProps) {
+  return (
+    <StyledInput
+      id={id}
+      type={type}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+    />
+  );
+}
