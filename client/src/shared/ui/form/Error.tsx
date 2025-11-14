b56825a085526afd5d3c2935df0883b31ab986c8
Eugene Taranov <tayev021@gmail.com>
2025-11-14 21:49:49 +0200

    feat(client): update shared Form.Error

--- client/src/shared/ui/form/Error.tsx
+++ client/src/shared/ui/form/Error.tsx
@@ -8,52 +8,97 @@ interface StyledErrorProps {
   $isRunning: boolean;
 }
 
-const runningLine = keyframes`
+interface ErrorContainerProps {
+  $hasError?: boolean;
+}
+
+const running = keyframes`
   0% { 
     transform: translateX(0); 
   }
-
   80% { 
-    transform: translateX(calc(-100% + 22rem)); 
+    transform: translateX(calc(-100% + 25rem));
   }
-
   100% { 
     transform: translateX(0); 
   }
 `;
 
-const ErrorContainer = styled.div`
+const runningShort = keyframes`
+  0% { 
+    transform: translateX(0); 
+  }
+  80% { 
+    transform: translateX(calc(-100% + 22rem)); 
+  }
+  100% { 
+    transform: translateX(0); 
+  }
+`;
+
+const ErrorContainer = styled.div<ErrorContainerProps>`
+  width: 100%;
+  padding: 0.1rem 0.6rem;
+  border-radius: 0.4rem;
+  font-size: 1.4rem;
+  line-height: 1;
+  color: var(--color-text-secondary);
+  background-color: var(--color-rose-500);
+
+  ${(props) => (props.$hasError ? '' : 'opacity: 0;')}
+
+  p {
+    animation: ${running} 5s linear infinite;
+  }
+`;
+
+const InputErrorContainer = styled(ErrorContainer)`
   width: 24rem;
   position: absolute;
   top: 100%;
   left: 1rem;
   transform: translateY(-40%);
-  padding: 0.1rem 0.6rem;
-  border-radius: 0.4rem;
   background-color: var(--color-primary);
-  font-size: 1.4rem;
-  line-height: 1;
-  white-space: nowrap;
-  overflow: hidden;
+
+  p {
+    animation: ${runningShort} 5s linear infinite;
+  }
 `;
 
-const StyledError = styled.p<StyledErrorProps>`
+const StyledError = styled.div<StyledErrorProps>`
+  white-space: nowrap;
+  overflow: hidden;
+
+  p {
+    ${(props) => (props.$isRunning ? '' : 'animation: none;')};
+  }
+`;
+
+const Message = styled.p`
   display: inline-block;
   position: relative;
-
-  color: var(--color-text-secondary);
-  animation: ${runningLine} 5s linear infinite;
-  ${(props) => (props.$isRunning ? '' : 'animation: none;')};
 `;
 
 export function Error({ message = '' }: ErrorProps) {
   if (message.length <= 0) return null;
 
   return (
-    <ErrorContainer>
-      <StyledError $isRunning={message.length > 29}>
-        <span>{message}</span>
+    <ErrorContainer $hasError={message.length > 0}>
+      <StyledError $isRunning={message.length > 36}>
+        <Message>{message}</Message>
       </StyledError>
     </ErrorContainer>
+  );
+}
+
+export function InputError({ message = '' }: ErrorProps) {
+  if (message.length <= 0) return null;
+
+  return (
+    <InputErrorContainer $hasError={message.length > 0}>
+      <StyledError $isRunning={message.length > 33}>
+        <Message>{message}</Message>
+      </StyledError>
+    </InputErrorContainer>
   );
 }
