cf33a359a5a80dee69fab07959571cffd5008209
Eugene Taranov <tayev021@gmail.com>
2025-11-14 21:53:16 +0200

    feat(client): update app router

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AppLayout } from '../layouts/AppLayout';
 import { SignupPage } from '../../pages/SignupPage';
+import { SigninPage } from '../../pages/SigninPage';
 
 export function AppRouter() {
   return (
@@ -8,7 +9,7 @@ export function AppRouter() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route index element={<div>Home page</div>} />
-          <Route path="signin" element={<div>sign in</div>} />
+          <Route path="signin" element={<SigninPage />} />
           <Route path="signup" element={<SignupPage />} />
           <Route path="*" element={<div>PageNotFound</div>} />
         </Route>
