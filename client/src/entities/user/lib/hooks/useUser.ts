7184784f50404d3e3bf896c3783e014c2d17154b
Eugene Taranov <tayev021@gmail.com>
2025-11-15 22:07:58 +0200

    feat(client): update useUser hook

--- client/src/entities/user/lib/hooks/useUser.ts
+++ client/src/entities/user/lib/hooks/useUser.ts
@@ -1,6 +1,9 @@
 import { useAppSelector } from '../../../../shared/lib/hooks/useAppSelector';
 
 export function useUser() {
-  const { user, isLoading, errors } = useAppSelector((state) => state.user);
-  return { user, isLoading, errors };
+  const { user, isLoading, initialized, errors } = useAppSelector(
+    (state) => state.user
+  );
+
+  return { user, isLoading, initialized, errors };
 }
