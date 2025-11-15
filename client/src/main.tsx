21144d81992f3a443868151281b5526f374fdadc
Eugene Taranov <tayev021@gmail.com>
2025-11-15 22:13:28 +0200

    feat(client): add AppStore initialization

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { store } from './app/store/index.ts';
+import { me } from './entities/user/model/thunks/me.ts';
 import { App } from './app/App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function init() {
+  await store.dispatch(me());
+}
+
+init().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
