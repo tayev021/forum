493b22def2a3bfe7874ef06c31378010d24e31e8
Eugene Taranov <tayev021@gmail.com>
2025-11-15 22:12:54 +0200

    feat(client): update app router

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,10 +1,17 @@
+import { useUser } from '../../entities/user/lib/hooks/useUser';
+import { AppLoader } from '../../shared/ui/AppLoader';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AppLayout } from '../layouts/AppLayout';
 import { SigninPage } from '../../pages/SigninPage';
 import { SignupPage } from '../../pages/SignupPage';
 import { PageNotFound } from '../../pages/PageNotFound';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export function AppRouter() {
+  const { initialized } = useUser();
+
+  if (!initialized) return <AppLoader />;
+
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +19,14 @@ export function AppRouter() {
           <Route index element={<div>Home page</div>} />
           <Route path="signin" element={<SigninPage />} />
           <Route path="signup" element={<SignupPage />} />
+          <Route
+            path="account"
+            element={
+              <ProtectedRoute>
+                <div>Account</div>
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
