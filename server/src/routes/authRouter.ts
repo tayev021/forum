632a99c45ce462374dfc09788c23292adafcb16d
Eugene Taranov <tayev021@gmail.com>
2025-11-15 22:00:52 +0200

    feat(server): update authRouter

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { validate } from '../middleware/validate';
 import { signupSchema } from '../validators/signupValidator';
 import { signinSchema } from '../validators/signinValidator';
-import { signin, signout, signup } from '../controllers/authController';
+import { me, signin, signout, signup } from '../controllers/authController';
+import { protect } from '../middleware/protect';
 
 const authRouter = Router();
 
+authRouter.get('/me', protect, me);
 authRouter.post('/signup', validate(signupSchema), signup);
 authRouter.post('/signin', validate(signinSchema), signin);
 authRouter.post('/signout', signout);
