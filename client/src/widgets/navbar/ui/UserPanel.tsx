aa8029b4bc486d43368c84dfb75c6f5688b8b4c2
Eugene Taranov <tayev021@gmail.com>
2025-11-16 10:34:08 +0200

    feat(client): update navbar widget

--- client/src/widgets/navbar/ui/UserPanel.tsx
+++ client/src/widgets/navbar/ui/UserPanel.tsx
@@ -2,13 +2,20 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router';
 import { useUser } from '../../../entities/user/lib/hooks/useUser';
 import { UserAvatar } from '../../../entities/user/ui/UserAvatar';
+import { Signout } from '../../../features/signout';
 
-const Container = styled.button`
+const Container = styled.div`
   display: flex;
-  gap: 1rem;
+  gap: 2rem;
   font-size: 1.8rem;
   line-height: 1;
   color: var(--color-text-secondary);
+`;
+
+const Account = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
   cursor: pointer;
 
   &:hover {
@@ -23,9 +30,12 @@ export function UserPanel() {
   if (!user) return null;
 
   return (
-    <Container onClick={() => navigate('/account')}>
-      <UserAvatar username={user.username} avatar={user.avatar} />
-      {user.username}
+    <Container>
+      <Account onClick={() => navigate('/account')}>
+        <UserAvatar username={user.username} avatar={user.avatar} />
+        {user.username}
+      </Account>
+      <Signout />
     </Container>
   );
 }
