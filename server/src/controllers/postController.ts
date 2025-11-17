bb25ff095bfafd1325769c7570a34ca5a4f8ebcf
Eugene Taranov <tayev021@gmail.com>
2025-11-17 12:21:53 +0200

    feat(server): update postController

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
+import { Thread } from '../models';
+import { AppError } from '../utils/AppError';
 import { Post } from '../models';
 
 export const createPost = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const threadId = Number(req.query.threadId);
+  const content = req.body.content;
+
+  const thread = await Thread.findByPk(threadId);
+
+  if (!thread) {
+    throw new AppError(
+      403,
+      'You are trying to post in a thread that does not exist.'
+    );
+  }
+
   const post = await Post.create({
-    authorId: req.user?.id,
-    threadId: req.threadId,
-    content: req.body.content,
+    authorId: user?.id,
+    threadId,
+    content,
   });
 
   res.status(200).json({ post });
