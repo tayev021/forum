73248b3a0b6122fbcc9fdcef7a5856d5773dd343
Eugene Taranov <tayev021@gmail.com>
2025-11-17 22:05:51 +0200

    feat(server): add ForumModel

---
+++ server/src/models/ForumModel.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import { database } from '../config/database';
+
+export class Forum extends Model {
+  public id!: number;
+  public authorId!: number;
+  public categoryId!: number;
+  public title!: string;
+  public createdAt!: string;
+}
+
+Forum.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    authorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+      onDelete: 'CASCADE',
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'categories', key: 'id' },
+      onDelete: 'CASCADE',
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: database,
+    modelName: 'Forum',
+    tableName: 'forums',
+    timestamps: true,
+  }
+);
