5ca1939407cee4a7555957ab5ed2eee3c10390ca
Eugene Taranov <tayev021@gmail.com>
2025-11-17 22:15:09 +0200

    feat(server): update rootRouter

--- server/src/routes/rootRouter.ts
+++ server/src/routes/rootRouter.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 import { authRouter } from './authRouter';
+import { forumRouter } from './forumRouter';
 import { threadRouter } from './threadRouter';
 import { postRouter } from './postRouter';
 import { notFound } from '../controllers/notFoundController';
+import { categoryRouter } from './categoryRouter';
 
 const rootRouter = Router();
 
 rootRouter.use('/auth', authRouter);
+rootRouter.use('/forums', forumRouter);
 rootRouter.use('/threads', threadRouter);
 rootRouter.use('/posts', postRouter);
+rootRouter.use('/categories', categoryRouter);
 rootRouter.use(notFound);
 
 export { rootRouter };
