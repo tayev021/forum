bd51705cd21f546b490bf82a5f75ba438e0cbefa
Eugene Taranov <tayev021@gmail.com>
2025-11-17 22:12:23 +0200

    feat(server): update threadController

--- server/src/controllers/threadController.ts
+++ server/src/controllers/threadController.ts
@@ -1,17 +1,33 @@
 import { Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
-import { Post, Thread } from '../models';
+import { Forum, Post, Thread } from '../models';
+import { AppError } from '../utils/AppError';
 
 export const createThread = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user!;
+  const title = req.body.title;
+  const content = req.body.content;
+  const forumId = Number(req.query.forumId);
+
+  const forum = await Forum.findByPk(forumId);
+
+  if (!forum) {
+    throw new AppError(
+      403,
+      'You are trying to create a thread in a forum that does not exist'
+    );
+  }
+
   const thread = await Thread.create({
-    title: req.body.title,
-    authorId: req.user?.id,
+    authorId: user.id,
+    forumId: forum.id,
+    title,
   });
 
   const post = await Post.create({
-    content: req.body.content,
-    authorId: req.user?.id,
+    authorId: user.id,
     threadId: thread.id,
+    content,
   });
 
   res.status(200).json({
