54a3b2365e2050afbc1aa3f43ee32504f371291d
Eugene Taranov <tayev021@gmail.com>
2025-11-17 22:02:35 +0200

    feat(server): add restrictTo middleware

---
+++ server/src/middleware/restrictTo.ts
@@ -0,0 +1,17 @@
+import { UserRole } from '../types/UserRole';
+import { NextFunction, Request, Response } from 'express';
+import { AppError } from '../utils/AppError';
+
+export function restrictTo(...roles: UserRole[]) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    const user = req.user!;
+
+    if (!roles.includes(user.role)) {
+      return next(
+        new AppError(403, "You don't have permission to perform this action.")
+      );
+    }
+
+    next();
+  };
+}
