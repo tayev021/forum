b41ea8c3670d190615030d5a230a6c1c9bb35c45
Eugene Taranov <tayev021@gmail.com>
2025-11-18 20:12:18 +0200

    feat(server): update postRouter

--- server/src/routes/postRouter.ts
+++ server/src/routes/postRouter.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { protect } from '../middleware/protect';
 import { validate } from '../middleware/validate';
 import { postSchema } from '../validators/postValidator';
-import { createPost } from '../controllers/postController';
+import { createPost, getLatestPosts } from '../controllers/postController';
 
 const postRouter = Router();
 
+postRouter.get('/latest', getLatestPosts);
 postRouter.post('/', protect, validate(postSchema), createPost);
 
 export { postRouter };
