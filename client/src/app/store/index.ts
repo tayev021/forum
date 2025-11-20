81c940e7450f65920fe963e798f08911536bdea1
Eugene Taranov <tayev021@gmail.com>
2025-11-20 11:32:58 +0200

    feat(client): update app store

--- client/src/app/store/index.ts
+++ client/src/app/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from '../../entities/user';
+import { postReducer } from '../../entities/post';
 import { statisticReducer } from '../../entities/statistic';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    post: postReducer,
     statistic: statisticReducer,
   },
 });
