03b38940c6bf2257962abce70b6f462a4465e950
Eugene Taranov <tayev021@gmail.com>
2025-11-20 11:26:19 +0200

    feat(client): update UserAvatar ui

--- client/src/entities/user/ui/UserAvatar.tsx
+++ client/src/entities/user/ui/UserAvatar.tsx
@@ -3,37 +3,40 @@ import styled from 'styled-components';
 interface UserAvatarProps {
   username: string;
   avatar: string | null;
+  size?: number;
 }
 
-const Avatar = styled.div`
-  width: 2rem;
-  height: 2rem;
+interface AvatarProps {
+  $size: number;
+}
+
+const Avatar = styled.div<AvatarProps>`
+  width: ${(props) => props.$size}rem;
+  height: ${(props) => props.$size}rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-transform: uppercase;
   border: 1px solid var(--color-text-secondary);
   border-radius: 50%;
-  font-size: 1.2rem;
+  font-size: ${(props) => props.$size * 0.4}rem;
   font-weight: 600;
   line-height: 1;
+  text-transform: uppercase;
   color: var(--color-primary);
   background-color: var(--color-text-secondary);
 `;
-
-const Placeholder = styled.div``;
 
 const Image = styled.img`
   width: 100%;
 `;
 
-export function UserAvatar({ username, avatar }: UserAvatarProps) {
+export function UserAvatar({ username, avatar, size = 2 }: UserAvatarProps) {
   return (
-    <Avatar>
+    <Avatar $size={size}>
       {avatar ? (
         <Image src={avatar} alt={`${username} avatar`} />
       ) : (
-        <Placeholder>{username.slice(0, 2)}</Placeholder>
+        username.slice(0, 2)
       )}
     </Avatar>
   );
