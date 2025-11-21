4e5d94bb32658835d900e29a3bfd353be8aba31f
Eugene Taranov <tayev021@gmail.com>
2025-11-21 18:36:32 +0200

    feat(server): update database associations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,25 +6,25 @@ import { Forum } from './ForumModel';
 import { Category } from './CategoryModel';
 import { User } from './UserModel';
 
-User.hasMany(Post, { foreignKey: 'authorId' });
+User.hasMany(Post, { foreignKey: 'authorId', as: 'posts' });
 Post.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 
-User.hasMany(Thread, { foreignKey: 'authorId' });
-Thread.belongsTo(User, { foreignKey: 'authorId' });
+User.hasMany(Thread, { foreignKey: 'authorId', as: 'threads' });
+Thread.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 
-User.hasMany(Forum, { foreignKey: 'authorId' });
-Forum.belongsTo(User, { foreignKey: 'authorId' });
+User.hasMany(Forum, { foreignKey: 'authorId', as: 'forums' });
+Forum.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 
-User.hasMany(Category, { foreignKey: 'authorId' });
-Category.belongsTo(User, { foreignKey: 'authorId' });
+User.hasMany(Category, { foreignKey: 'authorId', as: 'categories' });
+Category.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 
-Thread.hasMany(Post, { foreignKey: 'threadId' });
+Thread.hasMany(Post, { foreignKey: 'threadId', as: 'posts' });
 Post.belongsTo(Thread, { foreignKey: 'threadId', as: 'thread' });
 
-Forum.hasMany(Thread, { foreignKey: 'forumId' });
-Thread.belongsTo(Forum, { foreignKey: 'forumId' });
+Forum.hasMany(Thread, { foreignKey: 'forumId', as: 'treads' });
+Thread.belongsTo(Forum, { foreignKey: 'forumId', as: 'forum' });
 
-Category.hasMany(Forum, { foreignKey: 'categoryId' });
-Forum.belongsTo(Category, { foreignKey: 'categoryId' });
+Category.hasMany(Forum, { foreignKey: 'categoryId', as: 'forums' });
+Forum.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
 
 export { database, ErrorLog, User, Category, Forum, Thread, Post };
