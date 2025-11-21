adb5c96f6bc89b4100def6901570806da1a1a270
Eugene Taranov <tayev021@gmail.com>
2025-11-21 18:54:27 +0200

    feat(client): add HomePage

---
+++ client/src/pages/HomePage/ui/HomePage.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { Container } from '../../../shared/ui/Container';
+import { CategoriesWidget } from '../../../widgets/CategoriesWidget';
+import { StatisticWidget } from '../../../widgets/StatisticWidget';
+import { LatestPostsWidget } from '../../../widgets/LatestPostsWidget';
+
+const StyledContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: minmax(30rem, 1fr) 22rem;
+  gap: 2rem;
+  padding: 4rem 2rem;
+`;
+
+const Main = styled.div``;
+
+const SideBar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export function HomePage() {
+  return (
+    <StyledContainer>
+      <Main>
+        <CategoriesWidget />
+      </Main>
+      <SideBar>
+        <StatisticWidget />
+        <LatestPostsWidget />
+      </SideBar>
+    </StyledContainer>
+  );
+}
