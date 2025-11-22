ce84901f943272f7ec8da764052766bad424d9a0
Eugene Taranov <tayev021@gmail.com>
2025-11-22 21:34:35 +0200

    feat(server): update categoryController

--- server/src/controllers/categoryController.ts
+++ server/src/controllers/categoryController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { Category, Forum } from '../models';
+import { AppError } from '../utils/AppError';
 
 export const getCategories = catchAsync(async (req: Request, res: Response) => {
   const categories = await Category.findAll({
@@ -22,16 +23,50 @@ export const createCategory = catchAsync(
     const user = req.user!;
     const title = req.body.title;
 
-    const category = await Category.create({
+    await Category.create({
       authorId: user.id,
-      title,
+      title: `${title[0].toUpperCase()}${title.slice(1).toLowerCase()}`,
     });
 
-    res.status(200).json({
-      category: {
-        id: category.id,
-        title: category.title,
-      },
+    const categories = await Category.findAll({
+      attributes: ['id', 'title'],
+      include: [
+        {
+          model: Forum,
+          as: 'forums',
+          attributes: ['id', 'title'],
+        },
+      ],
     });
+
+    res.status(200).json({ categories });
+  }
+);
+
+export const deleteCategory = catchAsync(
+  async (req: Request, res: Response) => {
+    const category = await Category.findByPk(req.params.categoryId);
+
+    if (!category) {
+      throw new AppError(
+        400,
+        'You are trying to delete a category that does not exist'
+      );
+    }
+
+    await category.destroy();
+
+    const categories = await Category.findAll({
+      attributes: ['id', 'title'],
+      include: [
+        {
+          model: Forum,
+          as: 'forums',
+          attributes: ['id', 'title'],
+        },
+      ],
+    });
+
+    res.status(200).json({ categories });
   }
 );
