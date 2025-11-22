299f1ea005ed426fe38e8bfc282456db14041d5a
Eugene Taranov <tayev021@gmail.com>
2025-11-22 22:22:25 +0200

    feat(client): update category entity

--- client/src/entities/category/index.ts
+++ client/src/entities/category/index.ts
@@ -1,4 +1,6 @@
 import { categoryReducer } from './model/slice/categorySlice';
 import { useCategories } from './lib/hooks/useCategories';
+import { addCategory } from './model/thunks/addCategory';
+import { deleteCategory } from './model/thunks/deleteCategory';
 
-export { categoryReducer, useCategories };
+export { categoryReducer, useCategories, addCategory, deleteCategory };
