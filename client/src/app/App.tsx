acb2a66a1290da8c96533d7624e2f4a339272b82
Eugene Taranov <tayev021@gmail.com>
2025-11-24 22:36:55 +0200

    feat(client): update app

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,12 +1,14 @@
 import { Providers } from './providers';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { AppRouter } from './routes';
+import { Toaster } from '../shared/ui/Toaster';
 
 export function App() {
   return (
     <Providers>
       <GlobalStyles />
       <AppRouter />
+      <Toaster />
     </Providers>
   );
 }
