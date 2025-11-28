04b3cba6c91c6f7562ae1e8aad720175f03938aa
Eugene Taranov <tayev021@gmail.com>
2025-11-28 21:41:34 +0200

    feat(client): add shared paginate util

---
+++ client/src/shared/lib/utils/paginate.ts
@@ -0,0 +1,15 @@
+export function paginate(currentPage: number, totalPages: number, range = 1) {
+  const pages: (number | null)[] = [];
+  const left = currentPage - range;
+  const right = currentPage + range;
+
+  for (let i = 1; i <= totalPages; i++) {
+    if (i === 1 || i === totalPages || (i >= left && i <= right)) {
+      pages.push(i);
+    } else if (pages[pages.length - 1] !== null) {
+      pages.push(null);
+    }
+  }
+
+  return pages;
+}
