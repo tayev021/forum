10e7b70e8b7387a8e8d424f871faead06e837889
Eugene Taranov <tayev021@gmail.com>
2025-11-30 19:52:40 +0200

    feat(client): update ForumWidget

--- client/src/widgets/ForumWidget/ui/ForumWidget.tsx
+++ client/src/widgets/ForumWidget/ui/ForumWidget.tsx
@@ -2,17 +2,24 @@ import styled from 'styled-components';
 import { WidgetContainer } from '../../../shared/ui/WidgetContainer';
 import { WidgetLoader } from '../../../shared/ui/WidgetLoader';
 import { useCurrentForum } from '../lib/hooks/useCurrentForum';
+import { useRestrictTo } from '../../../entities/user';
 import { NoThreads } from './NoThreads';
 import { ThreadsList } from './ThreadsList';
 import { BackHomeButton } from '../../../shared/ui/BackHomeButton';
 import { HiArrowLeft } from 'react-icons/hi2';
 import { Pagination } from '../../../shared/ui/Pagination';
+import { Modal } from '../../../shared/ui/modal';
+import {
+  DeleteForumButton,
+  DeleteForumForm,
+} from '../../../features/deleteForum';
 
 const Header = styled.header`
-  display: flex;
+  display: grid;
+  grid-template-columns: min-content 1fr min-content;
   align-items: center;
   gap: 2rem;
-  padding: 1rem;
+  padding: 1rem 2rem 1rem 1rem;
   border-bottom: 1px solid var(--color-grey-400);
 `;
 
@@ -34,8 +41,13 @@ const Heading = styled.h3`
   color: var(--color-primary);
 `;
 
+const HeaderActions = styled.div`
+  justify-self: flex-end;
+`;
+
 export function ForumWidget() {
   const { forum, isLoading } = useCurrentForum();
+  const hasAdminsPermissions = useRestrictTo(['admin']);
 
   if (!forum || isLoading) {
     return <WidgetLoader />;
@@ -49,6 +61,21 @@ export function ForumWidget() {
             <BackIcon />
           </BackHomeButton>
           <Heading>{forum?.title} Forum</Heading>
+          <HeaderActions>
+            {hasAdminsPermissions && forum.threads.length === 0 && (
+              <>
+                <Modal.Open windowName={`deleteForum-${forum.id}`}>
+                  <DeleteForumButton />
+                </Modal.Open>
+                <Modal.Window name={`deleteForum-${forum.id}`}>
+                  <DeleteForumForm
+                    forumId={forum.id}
+                    forumTitle={forum.title}
+                  />
+                </Modal.Window>
+              </>
+            )}
+          </HeaderActions>
         </Header>
         {!forum || forum.threads.length === 0 ? (
           <NoThreads />
