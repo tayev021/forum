5c4b4a95b4083abe77445d72934618a3ada5f111
Eugene Taranov <tayev021@gmail.com>
2025-11-30 19:48:17 +0200

    feat(client): add deleteForum thunk

---
+++ client/src/entities/forum/model/thunks/deleteForum.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { API_URL } from '../../../../shared/constants';
+
+export const deleteForum = createAsyncThunk<
+  void,
+  { forumId: number },
+  { rejectValue: ServerError }
+>('forum/deleteForum', async function ({ forumId }, thunkAPI) {
+  const response = await fetch(`${API_URL}/forums/${forumId}`, {
+    method: 'delete',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
+  }
+});
