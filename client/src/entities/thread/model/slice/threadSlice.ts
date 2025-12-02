69ebdd664da72779271678e4c9fffcca614b02d8
Eugene Taranov <tayev021@gmail.com>
2025-12-02 15:30:57 +0200

    feat(client): add threadSlice

---
+++ client/src/entities/thread/model/slice/threadSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import type { Thread } from '../types/Thread';
+import { getThread } from '../thunks/getThread';
+
+interface ThreadState {
+  thread: Thread | null;
+  isLoading: boolean;
+  error: ServerError | null;
+}
+
+const initialState: ThreadState = {
+  thread: null,
+  isLoading: false,
+  error: null,
+};
+
+const threadSlice = createSlice({
+  name: 'thread',
+  initialState,
+  reducers: {
+    clearThreadError: (state) => {
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getThread.pending, (state, _action) => {
+      state.thread = null;
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      getThread.fulfilled,
+      (state, action: PayloadAction<Thread>) => {
+        state.thread = action.payload;
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(getThread.rejected, (state, action) => {
+      state.isLoading = false;
+
+      if (action.payload) {
+        state.error = action.payload;
+      } else {
+        state.error = { type: 'general', message: 'Unknown error!' };
+      }
+    });
+  },
+});
+
+export const threadReducer = threadSlice.reducer;
+export const { clearThreadError } = threadSlice.actions;
