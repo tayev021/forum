42d86928d42be9c318527e133b5b06962b46c1bd
Eugene Taranov <tayev021@gmail.com>
2025-12-04 14:00:14 +0200

    feat(client): update postSlice

--- client/src/entities/post/model/slice/postSlice.ts
+++ client/src/entities/post/model/slice/postSlice.ts
@@ -1,21 +1,29 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { ServerError } from '../../../../shared/types/ServerError';
 import type { LatestPost } from '../types/LatestPost';
 import { getLatestPosts } from '../thunks/getLatestPosts';
+import { createPost } from '../thunks/createPost';
 
 interface PostState {
   latestPosts: LatestPost[];
   isLoading: boolean;
+  error: ServerError | null;
 }
 
 const initialState: PostState = {
   latestPosts: [],
   isLoading: false,
+  error: null,
 };
 
 const postSlice = createSlice({
   name: 'post',
   initialState,
-  reducers: {},
+  reducers: {
+    clearPostError(state) {
+      state.error = null;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getLatestPosts.pending, (state, _action) => {
       state.isLoading = true;
@@ -31,7 +39,25 @@ const postSlice = createSlice({
       state.latestPosts = [];
       state.isLoading = false;
     });
+
+    builder.addCase(createPost.pending, (state, _action) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(createPost.fulfilled, (state, _action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(createPost.rejected, (state, action) => {
+      state.isLoading = false;
+
+      if (action.payload) {
+        state.error = action.payload;
+      } else {
+        state.error = { type: 'general', message: 'Unknown error!' };
+      }
+    });
   },
 });
 
 export const postReducer = postSlice.reducer;
+export const { clearPostError } = postSlice.actions;
