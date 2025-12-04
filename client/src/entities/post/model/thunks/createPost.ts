f28b2da541117f69a8be9e3d7310c954ed1c003a
Eugene Taranov <tayev021@gmail.com>
2025-12-04 13:59:03 +0200

    feat(client): add createPost thunk

---
+++ client/src/entities/post/model/thunks/createPost.ts
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import type { PostData } from '../types/PostData';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { API_URL } from '../../../../shared/constants';
+
+export const createPost = createAsyncThunk<
+  void,
+  PostData,
+  { rejectValue: ServerError }
+>('post/createPost', async function ({ threadId, content }, thunkAPI) {
+  const response = await fetch(`${API_URL}/posts?threadId=${threadId}`, {
+    method: 'post',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
+  }
+});
