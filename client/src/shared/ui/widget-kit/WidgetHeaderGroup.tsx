97ef2660069669ffbc6098c90a32aa714de05812
Eugene Taranov <tayev021@gmail.com>
2025-12-05 14:56:18 +0200

    feat(client): add shared WidgetHeaderGroup ui

---
+++ client/src/shared/ui/widget-kit/WidgetHeaderGroup.tsx
@@ -0,0 +1,22 @@
+import type { ReactElement } from 'react';
+import styled from 'styled-components';
+
+interface WidgetHeaderGroupProps {
+  className?: string;
+  children: ReactElement | false | (ReactElement | false)[];
+}
+
+const Group = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export function WidgetHeaderGroup({
+  className,
+  children,
+}: WidgetHeaderGroupProps) {
+  if (!children) return null;
+
+  return <Group className={className}>{children}</Group>;
+}
