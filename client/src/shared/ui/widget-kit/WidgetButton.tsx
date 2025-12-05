1b1b4d68dc16fe0aea2090f90a5e4c35d5f9356d
Eugene Taranov <tayev021@gmail.com>
2025-12-05 14:50:32 +0200

    feat(client): add shared WidgetButton ui

---
+++ client/src/shared/ui/widget-kit/WidgetButton.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const WidgetButton = styled.button`
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-secondary);
+  cursor: pointer;
+
+  &:hover {
+    color: var(--color-primary);
+  }
+`;
