faad65e8a0c452a966256bc6b23acd5daad10027
Eugene Taranov <tayev021@gmail.com>
2025-12-06 10:00:38 +0200

    feat(client): update threadSlice

--- client/src/entities/thread/model/slice/threadSlice.ts
+++ client/src/entities/thread/model/slice/threadSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { ServerError } from '../../../../shared/types/ServerError';
 import type { Thread } from '../types/Thread';
 import { getThread } from '../thunks/getThread';
+import { createThread } from '../thunks/createThread';
 
 interface ThreadState {
   thread: Thread | null;
@@ -37,6 +38,28 @@ const threadSlice = createSlice({
       }
     );
     builder.addCase(getThread.rejected, (state, action) => {
+      state.isLoading = false;
+
+      if (action.payload) {
+        state.error = action.payload;
+      } else {
+        state.error = { type: 'general', message: 'Unknown error!' };
+      }
+    });
+
+    builder.addCase(createThread.pending, (state, _action) => {
+      state.thread = null;
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      createThread.fulfilled,
+      (state, action: PayloadAction<Thread>) => {
+        state.thread = action.payload;
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(createThread.rejected, (state, action) => {
       state.isLoading = false;
 
       if (action.payload) {
