0e69bd77c8d17afa84673fab0cf284f1a473fb0e
Eugene Taranov <tayev021@gmail.com>
2025-12-07 09:41:08 +0200

    feat(client): update thread entity

--- client/src/entities/thread/index.ts
+++ client/src/entities/thread/index.ts
@@ -1,6 +1,7 @@
 import { clearThreadError, threadReducer } from './model/slice/threadSlice';
 import { getThread } from './model/thunks/getThread';
 import { createThread } from './model/thunks/createThread';
+import { deleteThread } from './model/thunks/deleteThread';
 import { useThread } from './lib/hooks/useThread';
 import { type Thread } from './model/types/Thread';
 
@@ -9,6 +10,7 @@ export {
   clearThreadError,
   getThread,
   createThread,
+  deleteThread,
   useThread,
   type Thread,
 };
