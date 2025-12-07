7459e60ff5aa8b7ae7403fd5d02cd67fca1683d1
Eugene Taranov <tayev021@gmail.com>
2025-12-07 09:39:58 +0200

    feat(client): add deleteThread thunk

---
+++ client/src/entities/thread/model/thunks/deleteThread.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { API_URL } from '../../../../shared/constants';
+
+export const deleteThread = createAsyncThunk<
+  void,
+  { threadId: number },
+  { rejectValue: ServerError }
+>('thread/deleteThread', async function ({ threadId }, thunkAPI) {
+  const response = await fetch(`${API_URL}/threads/${threadId}`, {
+    method: 'delete',
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
+  }
+});
