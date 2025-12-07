f473f996ee63892e6ee95b17feb0f4c9c7e7266a
Eugene Taranov <tayev021@gmail.com>
2025-12-07 09:39:28 +0200

    fix(client): fix createThread thunk

--- client/src/entities/thread/model/thunks/createThread.ts
+++ client/src/entities/thread/model/thunks/createThread.ts
@@ -8,22 +8,25 @@ export const createThread = createAsyncThunk<
   Thread,
   ThreadData,
   { rejectValue: ServerError }
->('forum/createForum', async function ({ forumId, title, content }, thunkAPI) {
-  const response = await fetch(`${API_URL}/threads?forumId=${forumId}`, {
-    method: 'post',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title, content }),
-  });
+>(
+  'thread/createThread',
+  async function ({ forumId, title, content }, thunkAPI) {
+    const response = await fetch(`${API_URL}/threads?forumId=${forumId}`, {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, content }),
+    });
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+
+    const json: { thread: Thread } = await response.json();
+
+    return json.thread;
   }
-
-  const json: { thread: Thread } = await response.json();
-
-  return json.thread;
-});
+);
