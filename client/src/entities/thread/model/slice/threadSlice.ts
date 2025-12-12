8a6be3c4809de997f91a153b75e73f915a9699a8
Eugene Taranov <tayev021@gmail.com>
2025-12-12 15:39:03 +0200

    feat(client): update threadSlice

--- client/src/entities/thread/model/slice/threadSlice.ts
+++ client/src/entities/thread/model/slice/threadSlice.ts
@@ -4,6 +4,7 @@ import type { Thread } from '../types/Thread';
 import { getThread } from '../thunks/getThread';
 import { createThread } from '../thunks/createThread';
 import { deleteThread } from '../thunks/deleteThread';
+import { updateThread } from '../thunks/updateThread';
 
 interface ThreadState {
   thread: Thread | null;
@@ -61,6 +62,26 @@ const threadSlice = createSlice({
       }
     );
     builder.addCase(createThread.rejected, (state, action) => {
+      state.isLoading = false;
+
+      if (action.payload) {
+        state.error = action.payload;
+      } else {
+        state.error = { type: 'general', message: 'Unknown error!' };
+      }
+    });
+
+    builder.addCase(updateThread.pending, (state, _action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(
+      updateThread.fulfilled,
+      (state, action: PayloadAction<Thread>) => {
+        state.thread = action.payload;
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(updateThread.rejected, (state, action) => {
       state.isLoading = false;
 
       if (action.payload) {
