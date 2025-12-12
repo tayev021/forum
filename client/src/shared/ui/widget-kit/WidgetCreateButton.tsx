083e6b96c76b03112cbd2076deeb973d574df696
Eugene Taranov <tayev021@gmail.com>
2025-12-12 15:25:37 +0200

    feat(client): update shared WidgetCreateButton ui

--- client/src/shared/ui/widget-kit/WidgetCreateButton.tsx
+++ client/src/shared/ui/widget-kit/WidgetCreateButton.tsx
@@ -2,20 +2,22 @@ import styled from 'styled-components';
 import { HiPlus } from 'react-icons/hi2';
 import { WidgetButton } from './WidgetButton';
 
-interface BackButtonProps {
+interface WidgetCreateButtonProps {
   className?: string;
   onClick?: () => void;
 }
 
 const Icon = styled(HiPlus)`
-  width: 100%;
-  height: 100%;
+  max-width: 2rem;
+  max-height: 2rem;
+  min-width: 1rem;
+  min-height: 1rem;
 `;
 
 export function WidgetCreateButton({
   className,
   onClick = () => {},
-}: BackButtonProps) {
+}: WidgetCreateButtonProps) {
   return (
     <WidgetButton className={className} onClick={onClick}>
       <Icon />
