fbe220fdf738c23c1e747a89aae42a26028bc0ed
Eugene Taranov <tayev021@gmail.com>
2025-12-13 22:31:48 +0200

    refactor(client): rework useCategories hook

--- client/src/entities/category/lib/hooks/useCategories.ts
+++ client/src/entities/category/lib/hooks/useCategories.ts
@@ -1,17 +1,9 @@
-import { useEffect } from 'react';
 import { useAppSelector } from '../../../../shared/lib/hooks/useAppSelector';
-import { useAppDispatch } from '../../../../shared/lib/hooks/useAppDispatch';
-import { getCategories } from '../../model/thunks/getCategories';
 
 export function useCategories() {
   const { categories, isLoading, error } = useAppSelector(
     (state) => state.category
   );
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(getCategories());
-  }, [dispatch]);
 
   return { categories, isLoading, error };
 }
