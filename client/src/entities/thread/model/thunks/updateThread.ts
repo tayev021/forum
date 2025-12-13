98dd1f8ea56627d92228f300c14d0c9969677bb7
Eugene Taranov <tayev021@gmail.com>
2025-12-13 21:47:20 +0200

    feat(client): update updateThread thunk

--- client/src/entities/thread/model/thunks/updateThread.ts
+++ client/src/entities/thread/model/thunks/updateThread.ts
@@ -5,24 +5,30 @@ import { API_URL } from '../../../../shared/constants';
 
 export const updateThread = createAsyncThunk<
   Thread,
-  { threadId: number; title: string },
+  { threadId: number; page?: number; title: string },
   { rejectValue: ServerError }
->('thread/updateThread', async function ({ threadId, title }, thunkAPI) {
-  const response = await fetch(`${API_URL}/threads/${threadId}`, {
-    method: 'PATCH',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title }),
-  });
+>(
+  'thread/updateThread',
+  async function ({ threadId, page = 1, title }, thunkAPI) {
+    const response = await fetch(
+      `${API_URL}/threads/${threadId}?page=${page}`,
+      {
+        method: 'PATCH',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title }),
+      }
+    );
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+
+    const json: { thread: Thread } = await response.json();
+
+    return json.thread;
   }
-
-  const json: { thread: Thread } = await response.json();
-
-  return json.thread;
-});
+);
