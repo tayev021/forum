efa5f7a536d737f78479e7bb9dab39f0a0ea6fd1
Eugene Taranov <tayev021@gmail.com>
2025-12-16 16:43:53 +0200

    feat(client): update PostItem ui

--- client/src/widgets/ThreadWidget/ui/posts/PostItem.tsx
+++ client/src/widgets/ThreadWidget/ui/posts/PostItem.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import type { Post } from '../../model/types/Post';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PostUpdate } from './PostUpdate';
 import { PostAuthor } from './PostAuthor';
 import { PostContent } from './PostContent';
@@ -10,7 +10,11 @@ interface PostItemProps {
   post: Post;
 }
 
-const StyledPostItem = styled.li`
+interface StyledPostItemProps {
+  id: number;
+}
+
+const StyledPostItem = styled.li<StyledPostItemProps>`
   display: grid;
   grid-template-columns: 18rem minmax(32rem, 1fr);
   border: 1px solid var(--color-grey-300);
@@ -22,12 +26,26 @@ export function PostItem({ post }: PostItemProps) {
   const [isUpdating, setIsUpdating] = useState(false);
   const ref = useOutsideClick(() => setIsUpdating(false));
 
+  useEffect(() => {
+    if (window.location.hash) {
+      const id = window.location.hash.slice(1);
+      const element = document.getElementById(id);
+
+      if (element) {
+        window.scrollTo({
+          top: element.offsetTop - 50,
+          behavior: 'smooth',
+        });
+      }
+    }
+  }, []);
+
   if (isUpdating) {
     return <PostUpdate ref={ref} postId={post.id} postContent={post.content} />;
   }
 
   return (
-    <StyledPostItem>
+    <StyledPostItem id={post.id}>
       <PostAuthor author={post.author} />
       <PostContent
         post={post}
