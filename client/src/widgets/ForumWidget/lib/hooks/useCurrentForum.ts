446fec49ee7096528061cfecbe76e60e9fcac0cd
Eugene Taranov <tayev021@gmail.com>
2025-12-19 21:26:10 +0200

    feat(client): update useCurrentForum hook

--- client/src/widgets/ForumWidget/lib/hooks/useCurrentForum.ts
+++ client/src/widgets/ForumWidget/lib/hooks/useCurrentForum.ts
@@ -2,7 +2,11 @@ import { useParams, useSearchParams } from 'react-router';
 import {
   clearForumError,
   getForum,
+  sortKeys,
+  sortOrders,
   useForum,
+  type SortKey,
+  type SortOrder,
 } from '../../../../entities/forum';
 import { useAppDispatch } from '../../../../shared/lib/hooks/useAppDispatch';
 import { useEffect } from 'react';
@@ -10,12 +14,28 @@ import toast from 'react-hot-toast';
 
 export function useCurrentForum() {
   const params = useParams();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const { forum, isLoading, error } = useForum();
   const dispatch = useAppDispatch();
 
   const forumId = Number(params.forumId);
   const page = Number(searchParams.get('page')) || 1;
+  const sortKey = searchParams.get('sortKey') as SortKey;
+  const sortOrder = searchParams.get('sortOrder') as SortOrder;
+
+  useEffect(() => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    if (!sortKey || !sortKeys.includes(sortKey)) {
+      newSearchParams.set('sortKey', sortKeys[0]);
+    }
+
+    if (!sortOrder || !sortOrders.includes(sortOrder)) {
+      newSearchParams.set('sortOrder', sortOrders[1]);
+    }
+
+    setSearchParams(newSearchParams);
+  }, [searchParams, setSearchParams, sortKey, sortOrder]);
 
   useEffect(() => {
     if (error?.type === 'general') {
@@ -26,9 +46,9 @@ export function useCurrentForum() {
 
   useEffect(() => {
     if (forumId) {
-      dispatch(getForum({ forumId, page }));
+      dispatch(getForum({ forumId, page, sortKey, sortOrder }));
     }
-  }, [dispatch, forumId, page]);
+  }, [dispatch, forumId, page, sortKey, sortOrder]);
 
   return { forum, isLoading, error };
 }
