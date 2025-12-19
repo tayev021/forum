b9ccf357759c329a2e1faa80e7b401ead2f621e1
Eugene Taranov <tayev021@gmail.com>
2025-12-19 21:22:17 +0200

    feat(client): update getForum thunk

--- client/src/entities/forum/model/thunks/getForum.ts
+++ client/src/entities/forum/model/thunks/getForum.ts
@@ -1,23 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { Forum } from '../types/Forum';
+import type { SortKey } from '../types/SortKey';
+import type { SortOrder } from '../types/SortOrder';
 import type { ServerError } from '../../../../shared/types/ServerError';
 import { API_URL } from '../../../../shared/constants';
 
 export const getForum = createAsyncThunk<
   Forum,
-  { forumId: number; page?: number },
+  { forumId: number; page?: number; sortKey?: SortKey; sortOrder?: SortOrder },
   { rejectValue: ServerError }
->('forum/getForum', async function ({ forumId, page = 1 }, thunkAPI) {
-  const response = await fetch(`${API_URL}/forums/${forumId}?page=${page}`, {
-    credentials: 'include',
-  });
+>(
+  'forum/getForum',
+  async function (
+    { forumId, page = 1, sortKey = 'updatedAt', sortOrder = 'DESC' },
+    thunkAPI
+  ) {
+    const response = await fetch(
+      `${API_URL}/forums/${forumId}?page=${page}&sortKey=${sortKey}&sortOrder=${sortOrder}`,
+      {
+        credentials: 'include',
+      }
+    );
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+
+    const json: { forum: Forum } = await response.json();
+
+    return json.forum;
   }
-
-  const json: { forum: Forum } = await response.json();
-
-  return json.forum;
-});
+);
