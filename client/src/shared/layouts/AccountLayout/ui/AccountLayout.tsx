c78a8e053a069d38d51e809e664f5d7824fbcc5b
Eugene Taranov <tayev021@gmail.com>
2025-12-20 21:16:47 +0200

    feat(client): add shared AccountLayout

---
+++ client/src/shared/layouts/AccountLayout/ui/AccountLayout.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { Container } from '../../../ui/Container';
+import type { ElementType } from 'react';
+import { Outlet } from 'react-router';
+
+interface AccountLayoutProps {
+  NavBar: ElementType;
+}
+
+const StyledContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 22rem minmax(30rem, 1fr);
+  gap: 2rem;
+  padding: 4rem 2rem;
+`;
+
+export function AccountLayout({ NavBar }: AccountLayoutProps) {
+  return (
+    <StyledContainer>
+      <NavBar />
+      <div>
+        <Outlet />
+      </div>
+    </StyledContainer>
+  );
+}
