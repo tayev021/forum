00fc5016f2722cd4ea5fbb22b7f762cbf6e52004
Eugene Taranov <tayev021@gmail.com>
2025-12-22 22:44:25 +0200

    feat(server): update app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { rootRouter } from './routes/rootRouter';
@@ -10,6 +11,7 @@ import { globalErrorHandler } from './controllers/errorController';
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.static(path.join(process.cwd(), 'public')));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
