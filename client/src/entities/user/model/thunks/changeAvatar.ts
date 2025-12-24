7de32a6f17feeb97311dce6d797adf6411ceb465
Eugene Taranov <tayev021@gmail.com>
2025-12-24 12:13:08 +0200

    feat(client): add changeAvatar thunk

---
+++ client/src/entities/user/model/thunks/changeAvatar.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import type { User } from '../types/User';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { API_URL } from '../../../../shared/constants';
+
+export const changeAvatar = createAsyncThunk<
+  User,
+  FormData,
+  { rejectValue: ServerError }
+>('user/changeAvatar', async function (formData, thunkAPI) {
+  const response = await fetch(`${API_URL}/users/avatar`, {
+    method: 'PATCH',
+    credentials: 'include',
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
+  }
+
+  const json: { user: User } = await response.json();
+
+  return json.user;
+});
