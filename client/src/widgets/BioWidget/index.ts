8a441068f861cf04e93e17d9684203a2f7e39f91
Eugene Taranov <tayev021@gmail.com>
2025-12-24 12:16:38 +0200

    feat(client): add BioWidget

---
+++ client/src/widgets/BioWidget/index.ts
@@ -0,0 +1,3 @@
+import { BioWidget } from './ui/BioWidget';
+
+export { BioWidget };
---
+++ client/src/widgets/BioWidget/ui/BioWidget.tsx
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import { Widget } from '../../../shared/ui/WidgetKit';
+import { useState } from 'react';
+import { useUser } from '../../../entities/user';
+import { UserAvatar } from '../../../entities/user/ui/UserAvatar';
+import { ChangeAvatar } from '../../../features/changeAvatar';
+import { HiPencilSquare } from 'react-icons/hi2';
+import { UpdateBioForm } from '../../../features/updateBio';
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  gap: 4rem;
+  padding: 2rem 4rem;
+
+  @media (max-width: 1100px) {
+    gap: 2rem;
+    padding: 2rem;
+  }
+
+  @media (max-width: 900px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, max-content);
+  }
+`;
+
+const AvatarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const StyledEditButton = styled(Widget.EditButton)`
+  width: 2.4rem;
+  height: 2.4rem;
+  float: right;
+  margin: 0 0 1rem 1rem;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const NoBio = styled.p`
+  padding: 2rem 0;
+  text-align: center;
+  font-style: italic;
+  color: var(--color-grey-500);
+`;
+
+export function BioWidget() {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <Widget.Container>
+      <Widget.Header>
+        <Widget.Title>Bio</Widget.Title>
+      </Widget.Header>
+      <Main>
+        <AvatarContainer>
+          <UserAvatar username={user.username} avatar={user.avatar} size={16} />
+          <ChangeAvatar />
+        </AvatarContainer>
+        <div>
+          {isUpdating ? (
+            <UpdateBioForm handleClose={() => setIsUpdating(false)} />
+          ) : (
+            <>
+              <StyledEditButton onClick={() => setIsUpdating(true)}>
+                <HiPencilSquare />
+              </StyledEditButton>
+              {user.bio ? (
+                <p>{user.bio}</p>
+              ) : (
+                <NoBio>
+                  You have not added any information about yourself yet
+                </NoBio>
+              )}
+            </>
+          )}
+        </div>
+      </Main>
+    </Widget.Container>
+  );
+}
