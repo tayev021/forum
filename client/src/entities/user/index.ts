dfae5cdc3e3d33f3ab3390473be7b55b1485e0b1
Eugene Taranov <tayev021@gmail.com>
2025-12-24 12:14:00 +0200

    feat(client): update user entity

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -2,6 +2,7 @@ import { clearUserError, userReducer } from './model/slice/userSlice';
 import { signup } from './model/thunks/signup';
 import { signin } from './model/thunks/signin';
 import { signout } from './model/thunks/signout';
+import { changeAvatar } from './model/thunks/changeAvatar';
 import { type SignupData } from './model/types/SignupData';
 import { useUser } from './lib/hooks/useUser';
 import { useRestrictTo } from './lib/hooks/useRestrictTo';
@@ -12,6 +13,7 @@ export {
   signup,
   signin,
   signout,
+  changeAvatar,
   type SignupData,
   useUser,
   useRestrictTo,
