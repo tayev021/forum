beacd3c24dafdc261a7d88b922b5ae62f47d912e
Eugene Taranov <tayev021@gmail.com>
2025-12-25 21:43:46 +0200

    feat(client): update post entity

--- client/src/entities/post/index.ts
+++ client/src/entities/post/index.ts
@@ -1,8 +1,11 @@
 import { clearPostError, postReducer } from './model/slice/postSlice';
 import { createPost } from './model/thunks/createPost';
 import { usePost } from './lib/hooks/usePost';
+import { useAuthorPosts } from './lib/hooks/useAuthorPosts';
 import { useLatestPosts } from './lib/hooks/useLatestPosts';
 import { updatePost } from './model/thunks/updatePost';
+import { getAuthorPosts } from './model/thunks/getAuthorPosts';
+import type { Post } from './model/types/Post';
 
 export {
   postReducer,
@@ -10,5 +13,8 @@ export {
   createPost,
   updatePost,
   usePost,
+  useAuthorPosts,
   useLatestPosts,
+  getAuthorPosts,
+  type Post,
 };
