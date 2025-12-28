8881984bc0e95cb680cea88c2d59b7c5070fb4ad
Eugene Taranov <tayev021@gmail.com>
2025-12-28 20:41:34 +0200

    feat(client): add changePassword thunk

---
+++ client/src/entities/user/model/thunks/changePassword.ts
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import type { User } from '../types/User';
+import type { PasswordData } from '../types/PasswordData';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { API_URL } from '../../../../shared/constants';
+
+export const changePassword = createAsyncThunk<
+  User,
+  PasswordData,
+  { rejectValue: ServerError }
+>('user/changePassword', async function (passwordData, thunkAPI) {
+  const response = await fetch(`${API_URL}/users/password`, {
+    method: 'PATCH',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(passwordData),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
+  }
+
+  const json: { user: User } = await response.json();
+
+  return json.user;
+});
