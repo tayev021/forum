58b2dacbc12e8632d695a1e73629bf113798ef94
Eugene Taranov <tayev021@gmail.com>
2026-01-04 12:54:12 +0200

    feat(client): update AccountSettingsPage

--- client/src/pages/account/settings/ui/AccountSettingsPage.tsx
+++ client/src/pages/account/settings/ui/AccountSettingsPage.tsx
@@ -1,9 +1,23 @@
+import styled from 'styled-components';
+import { useUser } from '../../../../entities/user';
 import { AccountPasswordWidget } from '../../../../widgets/AccountPasswordWidget';
+import { AccountDeleteWidget } from '../../../../widgets/AccountDeleteWidget';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
 
 export function AccountSettingsPage() {
+  const { user } = useUser();
+
+  if (!user) return null;
+
   return (
-    <>
+    <Container>
       <AccountPasswordWidget />
-    </>
+      {user.role === 'user' && <AccountDeleteWidget />}
+    </Container>
   );
 }
