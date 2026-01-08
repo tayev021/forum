8fed72ba594c5ff9c98b8a1f80b14edac14508e8
Eugene Taranov <tayev021@gmail.com>
2026-01-08 22:43:57 +0200

    feat(server): update database associations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,6 +5,7 @@ import { Thread } from './ThreadModel';
 import { Forum } from './ForumModel';
 import { Category } from './CategoryModel';
 import { User } from './UserModel';
+import { Subscription } from './SubscriptionModel';
 
 User.hasMany(Post, { foreignKey: 'authorId', as: 'posts' });
 Post.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
@@ -18,6 +19,14 @@ Forum.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 User.hasMany(Category, { foreignKey: 'authorId', as: 'categories' });
 Category.belongsTo(User, { foreignKey: 'authorId', as: 'author' });
 
+User.hasMany(Subscription, {
+  foreignKey: 'userId',
+  as: 'subscriptions',
+  onDelete: 'CASCADE',
+  hooks: true,
+});
+Subscription.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 Thread.hasMany(Post, {
   foreignKey: 'threadId',
   as: 'posts',
@@ -26,10 +35,27 @@ Thread.hasMany(Post, {
 });
 Post.belongsTo(Thread, { foreignKey: 'threadId', as: 'thread' });
 
+Thread.hasMany(Subscription, {
+  foreignKey: 'threadId',
+  as: 'subscriptions',
+  onDelete: 'CASCADE',
+  hooks: true,
+});
+Subscription.belongsTo(Thread, { foreignKey: 'threadId', as: 'thread' });
+
 Forum.hasMany(Thread, { foreignKey: 'forumId', as: 'threads' });
 Thread.belongsTo(Forum, { foreignKey: 'forumId', as: 'forum' });
 
 Category.hasMany(Forum, { foreignKey: 'categoryId', as: 'forums' });
 Forum.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
 
-export { database, ErrorLog, User, Category, Forum, Thread, Post };
+export {
+  database,
+  ErrorLog,
+  User,
+  Category,
+  Forum,
+  Thread,
+  Post,
+  Subscription,
+};
