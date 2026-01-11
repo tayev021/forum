7a471714c1c73f429d1739ee3c7ed0a857205771
Eugene Taranov <tayev021@gmail.com>
2026-01-11 22:27:15 +0200

    feat(client): update account pages

--- client/src/pages/account/index.ts
+++ client/src/pages/account/index.ts
@@ -1,11 +1,15 @@
 import { AccountBioPage } from './bio';
 import { AccountPostsPage } from './posts';
 import { AccountThreadsPage } from './threads';
+import { AccountSubscriptionsPage } from './subscriptions';
+import { AccountNotificationsPage } from './notifications';
 import { AccountSettingsPage } from './settings';
 
 export {
   AccountBioPage,
   AccountPostsPage,
   AccountThreadsPage,
+  AccountSubscriptionsPage,
+  AccountNotificationsPage,
   AccountSettingsPage,
 };
