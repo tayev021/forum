14747353a0093475c75e0f5c1e69af080793d29e
Eugene Taranov <tayev021@gmail.com>
2026-01-11 22:19:31 +0200

    feat(client): update user entity

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -2,19 +2,26 @@ import { clearUserError, userReducer } from './model/slice/userSlice';
 import { signup } from './model/thunks/signup';
 import { signin } from './model/thunks/signin';
 import { signout } from './model/thunks/signout';
+import { getUserPosts } from './model/thunks/getUserPosts';
 import { getUserThreads } from './model/thunks/getUserThreads';
+import { getUserSubscriptions } from './model/thunks/getUserSubscriptions';
+import { getUserNotifications } from './model/thunks/getUserNotifications';
+import { updateBio } from './model/thunks/updateBio';
 import { changePassword } from './model/thunks/changePassword';
 import { deleteAccount } from './model/thunks/deleteAccount';
 import { changeAvatar } from './model/thunks/changeAvatar';
 import { useUser } from './lib/hooks/useUser';
-import { useUserPosts } from './lib/hooks/useUserPosts';
-import { getUserPosts } from './model/thunks/getUserPosts';
-import { useUserThreads } from './lib/hooks/useUserThreads';
 import { useRestrictTo } from './lib/hooks/useRestrictTo';
+import { useUserPosts } from './lib/hooks/useUserPosts';
+import { useUserThreads } from './lib/hooks/useUserThreads';
+import { useUserSubscriptions } from './lib/hooks/useUserSubscriptions';
+import { useUserNotifications } from './lib/hooks/useUserNotifications';
 import { type SignupData } from './model/types/SignupData';
 import { type PasswordData } from './model/types/PasswordData';
 import { type UserPost } from './model/types/UserPost';
 import { type UserThread } from './model/types/UserThread';
+import { type UserSubscription } from './model/types/UserSubscription';
+import { type UserNotification } from './model/types/UserNotification';
 
 export {
   userReducer,
@@ -24,15 +31,22 @@ export {
   signout,
   getUserPosts,
   getUserThreads,
+  getUserSubscriptions,
+  getUserNotifications,
+  updateBio,
+  changeAvatar,
   changePassword,
   deleteAccount,
-  changeAvatar,
   useUser,
+  useRestrictTo,
   useUserPosts,
   useUserThreads,
-  useRestrictTo,
+  useUserSubscriptions,
+  useUserNotifications,
   type SignupData,
   type PasswordData,
   type UserPost,
   type UserThread,
+  type UserSubscription,
+  type UserNotification,
 };
