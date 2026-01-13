415f94ae970c0b561cc9e753e7ae68f7267e9738
Eugene Taranov <tayev021@gmail.com>
2026-01-13 12:45:25 +0200

    feat(client): update UserNotification type

--- client/src/entities/user/model/types/UserNotification.ts
+++ client/src/entities/user/model/types/UserNotification.ts
@@ -3,7 +3,7 @@ import type { Thread } from '../../../thread';
 export interface UserNotification {
   id: number;
   lastReadAt: string;
-  thread: Pick<Thread, 'id' | 'title'> & {
+  thread: Pick<Thread, 'id' | 'title' | 'page'> & {
     unreadPostId: number;
     unreadPostContent: string;
     unreadPostCreatedAt: string;
