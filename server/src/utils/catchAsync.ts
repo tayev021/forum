3029591fcdb217033f5dc2ee2adb12d07e7a5484
Eugene Taranov <tayev021@gmail.com>
2026-01-14 22:03:04 +0200

    feat(server): update catchAsync util

--- server/src/utils/catchAsync.ts
+++ server/src/utils/catchAsync.ts
@@ -4,6 +4,9 @@ export function catchAsync(
   fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
 ) {
   return function (req: Request, res: Response, next: NextFunction) {
-    Promise.resolve(fn(req, res, next)).catch(next);
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      console.log(`\x1b[31m[Server Error]:\x1b[33m ${err.message} \x1b[0m`);
+      next(err);
+    });
   };
 }
