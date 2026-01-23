e0fdecbd91fae954a183a8501e2d8a01a583f796
Eugene Taranov <tayev021@gmail.com>
2026-01-23 12:02:44 +0200

    feat(server): add reportSchemas

---
+++ server/src/validators/reportSchemas.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const reportSchema = z.object({
+  reason: z
+    .string()
+    .min(3, 'The report reason must be at least 3 characters long')
+    .max(1024, 'The report reason must be a maximum 1024 characters long'),
+});
