aaaa9526d387e87cb7a0ff9a873b561355722583
Eugene Taranov <tayev021@gmail.com>
2026-01-24 13:13:50 +0200

    feat(client): update reportPost feature

--- client/src/features/reportPost/ui/ReportPost.tsx
+++ client/src/features/reportPost/ui/ReportPost.tsx
@@ -56,6 +56,7 @@ export function ReportPost({ post, closeModal = () => {} }: ReportPostProps) {
       );
       dispatch(clearPost());
       closeModal();
+      toast.success('Your report has been received and will be reviewed');
     }
   }, [serverError, isLoading, serverPost, post]);
 
