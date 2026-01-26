509ad5734172a9a9b995dad003edd5ffaa0e7026
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:09:05 +0200

    feat(client): update getLatestPosts thunk

--- client/src/entities/post/model/thunks/getLatestPosts.ts
+++ client/src/entities/post/model/thunks/getLatestPosts.ts
@@ -1,22 +1,37 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { LatestPost } from '../types/LatestPost';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { API_URL } from '../../../../shared/constants';
 
 export const getLatestPosts = createAsyncThunk<
   LatestPost[],
   void,
-  { rejectValue: any }
+  { rejectValue: ServerError }
 >('post/getLatestPosts', async function (_, thunkAPI) {
-  const response = await fetch(`${API_URL}/posts/latest`, {
-    credentials: 'include',
-  });
+  try {
+    const response = await fetch(`${API_URL}/posts/latest`, {
+      credentials: 'include',
+    });
 
-  if (!response.ok) {
-    const errors = await response.json();
-    return thunkAPI.rejectWithValue(errors);
+    if (!response.ok) {
+      const errors = await response.json();
+      return thunkAPI.rejectWithValue(errors);
+    }
+
+    const json: { posts: LatestPost[] } = await response.json();
+
+    return json.posts;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'The server is not responding',
+      });
+    } else {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'Unknown error',
+      });
+    }
   }
-
-  const json: { posts: LatestPost[] } = await response.json();
-
-  return json.posts;
 });
