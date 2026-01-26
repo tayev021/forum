1f6be0c3b3499fa83df8fbb94493b9df016973ac
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:11:19 +0200

    feat(client): update getStatistic thunk

--- client/src/entities/statistic/model/thunks/getStatistic.ts
+++ client/src/entities/statistic/model/thunks/getStatistic.ts
@@ -1,22 +1,37 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { Statistic } from '../types/Statistic';
+import type { ServerError } from '../../../../shared/types/ServerError';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { API_URL } from '../../../../shared/constants';
 
 export const getStatistic = createAsyncThunk<
   Statistic,
   void,
-  { rejectValue: any }
+  { rejectValue: ServerError }
 >('statistic/getStatistic', async function (_, thunkAPI) {
-  const response = await fetch(`${API_URL}/statistic`, {
-    credentials: 'include',
-  });
+  try {
+    const response = await fetch(`${API_URL}/statistic`, {
+      credentials: 'include',
+    });
 
-  if (!response.ok) {
-    const errors = await response.json();
-    return thunkAPI.rejectWithValue(errors);
+    if (!response.ok) {
+      const errors = await response.json();
+      return thunkAPI.rejectWithValue(errors);
+    }
+
+    const statistic: Statistic = await response.json();
+
+    return statistic;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'The server is not responding',
+      });
+    } else {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'Unknown error',
+      });
+    }
   }
-
-  const statistic: Statistic = await response.json();
-
-  return statistic;
 });
