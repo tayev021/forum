7abef85159a0b287bd386f399757ed7cb45e55f0
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:15:51 +0200

    feat(client): update signout thunk

--- client/src/entities/user/model/thunks/signout.ts
+++ client/src/entities/user/model/thunks/signout.ts
@@ -7,13 +7,27 @@ export const signout = createAsyncThunk<
   void,
   { rejectValue: ServerError }
 >('user/signout', async function (_, thunkAPI) {
-  const response = await fetch(`${API_URL}/auth/signout`, {
-    method: 'POST',
-    credentials: 'include',
-  });
+  try {
+    const response = await fetch(`${API_URL}/auth/signout`, {
+      method: 'POST',
+      credentials: 'include',
+    });
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'The server is not responding',
+      });
+    } else {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'Unknown error',
+      });
+    }
   }
 });
