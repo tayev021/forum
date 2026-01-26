7e29c60c804b11760349724513a794e3fb99284d
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:06:33 +0200

    feat(client): update getCategories thunk

--- client/src/entities/category/model/thunks/getCategories.ts
+++ client/src/entities/category/model/thunks/getCategories.ts
@@ -8,16 +8,30 @@ export const getCategories = createAsyncThunk<
   void,
   { rejectValue: ServerError }
 >('category/getCategories', async function (_, thunkAPI) {
-  const response = await fetch(`${API_URL}/categories`, {
-    credentials: 'include',
-  });
+  try {
+    const response = await fetch(`${API_URL}/categories`, {
+      credentials: 'include',
+    });
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+
+    const json: { categories: Category[] } = await response.json();
+
+    return json.categories;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'The server is not responding',
+      });
+    } else {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'Unknown error',
+      });
+    }
   }
-
-  const json: { categories: Category[] } = await response.json();
-
-  return json.categories;
 });
