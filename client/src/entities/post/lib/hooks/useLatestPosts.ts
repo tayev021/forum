7a20618a12be2ee2790bd0e6ac9aa1341299c1ad
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:10:21 +0200

    feat(client): update useLatestPosts hook

--- client/src/entities/post/lib/hooks/useLatestPosts.ts
+++ client/src/entities/post/lib/hooks/useLatestPosts.ts
@@ -4,12 +4,14 @@ import { useAppDispatch } from '../../../../shared/lib/hooks/useAppDispatch';
 import { getLatestPosts } from '../../model/thunks/getLatestPosts';
 
 export function useLatestPosts() {
-  const { latestPosts, isLoading } = useAppSelector((state) => state.post);
+  const { latestPosts, isLoading, error } = useAppSelector(
+    (state) => state.post,
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getLatestPosts());
   }, [dispatch]);
 
-  return { latestPosts, isLoading };
+  return { latestPosts, isLoading, error };
 }
