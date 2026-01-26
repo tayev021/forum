ec8a39d457c22a7c5d6dc7da290e1b6b546044a1
Eugene Taranov <tayev021@gmail.com>
2026-01-26 15:12:53 +0200

    feat(client): update getThread thunk

--- client/src/entities/thread/model/thunks/getThread.ts
+++ client/src/entities/thread/model/thunks/getThread.ts
@@ -8,16 +8,33 @@ export const getThread = createAsyncThunk<
   { threadId: number; page?: number },
   { rejectValue: ServerError }
 >('thread/getThread', async function ({ threadId, page = 1 }, thunkAPI) {
-  const response = await fetch(`${API_URL}/threads/${threadId}?page=${page}`, {
-    credentials: 'include',
-  });
+  try {
+    const response = await fetch(
+      `${API_URL}/threads/${threadId}?page=${page}`,
+      {
+        credentials: 'include',
+      },
+    );
 
-  if (!response.ok) {
-    const error = await response.json();
-    return thunkAPI.rejectWithValue(error);
+    if (!response.ok) {
+      const error = await response.json();
+      return thunkAPI.rejectWithValue(error);
+    }
+
+    const json: { thread: Thread } = await response.json();
+
+    return json.thread;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'The server is not responding',
+      });
+    } else {
+      return thunkAPI.rejectWithValue({
+        type: 'general',
+        message: 'Unknown error',
+      });
+    }
   }
-
-  const json: { thread: Thread } = await response.json();
-
-  return json.thread;
 });
