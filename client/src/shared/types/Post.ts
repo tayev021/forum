cd5944a00794667185e7032679762ad5fc98e7d6
Eugene Taranov <tayev021@gmail.com>
2026-01-29 22:17:46 +0200

    feat(client): update Post type

--- client/src/shared/types/Post.ts
+++ client/src/shared/types/Post.ts
@@ -1,8 +1,11 @@
+import type { Attachment } from './Attachment';
+
 export interface Post {
   id: number;
   threadId: number;
   content: string;
   authorId: number | null;
+  attachments: Attachment[];
   likes: number;
   isLiked: boolean;
   isReported: boolean;
