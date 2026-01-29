35726562c8a293b72f541cae4197d1b7510e47cd
Eugene Taranov <tayev021@gmail.com>
2026-01-29 22:22:32 +0200

    fix(server): fix protect middleware

--- server/src/middleware/protect.ts
+++ server/src/middleware/protect.ts
@@ -1,28 +1,31 @@
+import { catchAsync } from '../utils/catchAsync';
 import { NextFunction, Request, Response } from 'express';
 import { AppError } from '../utils/AppError';
 import { verifyJwtAsync } from '../utils/verifyJwtAsync';
 import { User } from '../models';
 
-export async function protect(req: Request, res: Response, next: NextFunction) {
-  const token = req.cookies.token;
+export const protect = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.token;
 
-  if (!token) {
-    throw new AppError(401, 'Failed to pass protection!', {
-      type: 'general',
-      message: 'You are not signed in! Please sign in to get access',
-    });
+    if (!token) {
+      throw new AppError(401, 'Failed to pass protection!', {
+        type: 'general',
+        message: 'You are not signed in! Please sign in to get access',
+      });
+    }
+
+    const decoded = await verifyJwtAsync(token);
+    const currentUser = await User.findByPk(decoded.id);
+
+    if (!currentUser) {
+      throw new AppError(401, 'Failed to pass protection!', {
+        type: 'general',
+        message: 'This token belongs to a user who does not exist',
+      });
+    }
+
+    req.user = currentUser;
+    next();
   }
-
-  const decoded = await verifyJwtAsync(token);
-  const currentUser = await User.findByPk(decoded.id);
-
-  if (!currentUser) {
-    throw new AppError(401, 'Failed to pass protection!', {
-      type: 'general',
-      message: 'This token belongs to a user who does not exist',
-    });
-  }
-
-  req.user = currentUser;
-  next();
-}
+);
