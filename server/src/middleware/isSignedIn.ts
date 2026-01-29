1168918e08cf0184d443acb98fe45b59a6c4ebba
Eugene Taranov <tayev021@gmail.com>
2026-01-29 22:22:13 +0200

    fix(server): fix isSignedIn middleware

--- server/src/middleware/isSignedIn.ts
+++ server/src/middleware/isSignedIn.ts
@@ -1,30 +1,29 @@
+import { catchAsync } from '../utils/catchAsync';
 import { NextFunction, Request, Response } from 'express';
 import { verifyJwtAsync } from '../utils/verifyJwtAsync';
 import { User } from '../models';
 
-export async function isSignedIn(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const token = req.cookies.token;
+export const isSignedIn = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.token;
 
-  if (!token) {
-    req.isSignedIn = false;
-    req.user = null;
-    return next();
+    if (!token) {
+      req.isSignedIn = false;
+      req.user = null;
+      return next();
+    }
+
+    const decoded = await verifyJwtAsync(token);
+    const currentUser = await User.findByPk(decoded.id);
+
+    if (!currentUser) {
+      req.isSignedIn = false;
+      req.user = null;
+      return next();
+    }
+
+    req.isSignedIn = true;
+    req.user = currentUser;
+    next();
   }
-
-  const decoded = await verifyJwtAsync(token);
-  const currentUser = await User.findByPk(decoded.id);
-
-  if (!currentUser) {
-    req.isSignedIn = false;
-    req.user = null;
-    return next();
-  }
-
-  req.isSignedIn = true;
-  req.user = currentUser;
-  next();
-}
+);
