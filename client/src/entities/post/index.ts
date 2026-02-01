bc066eefc635d880a524e03ee8defde901ea0c1d
Eugene Taranov <tayev021@gmail.com>
2026-02-01 21:25:54 +0200

    feat(client): update post entity

--- client/src/entities/post/index.ts
+++ client/src/entities/post/index.ts
@@ -8,6 +8,7 @@ import { updatePost } from './model/thunks/updatePost';
 import { likePost } from './model/thunks/likePost';
 import { reportPost } from './model/thunks/reportPost';
 import { deletePost } from './model/thunks/deletePost';
+import { deletePostAttachment } from './model/thunks/deletePostAttachment';
 import { usePost } from './lib/hooks/usePost';
 import { useLatestPosts } from './lib/hooks/useLatestPosts';
 import type { Post } from './model/types/Post';
@@ -21,6 +22,7 @@ export {
   likePost,
   reportPost,
   deletePost,
+  deletePostAttachment,
   usePost,
   useLatestPosts,
   type Post,
