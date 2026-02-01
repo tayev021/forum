e185131c23c296ea453a7a94a50e39062ed95bc9
Eugene Taranov <tayev021@gmail.com>
2026-02-01 21:27:02 +0200

    feat(client): update createThread thunk

--- client/src/entities/thread/model/thunks/createThread.ts
+++ client/src/entities/thread/model/thunks/createThread.ts
@@ -8,25 +8,19 @@ export const createThread = createAsyncThunk<
   Thread,
   ThreadData,
   { rejectValue: ServerError }
->(
-  'thread/createThread',
-  async function ({ forumId, title, content }, thunkAPI) {
-    const response = await fetch(`${API_URL}/threads?forumId=${forumId}`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ title, content }),
-    });
+>('thread/createThread', async function ({ forumId, formData }, thunkAPI) {
+  const response = await fetch(`${API_URL}/threads?forumId=${forumId}`, {
+    method: 'POST',
+    credentials: 'include',
+    body: formData,
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      return thunkAPI.rejectWithValue(error);
-    }
-
-    const json: { thread: Thread } = await response.json();
-
-    return json.thread;
+  if (!response.ok) {
+    const error = await response.json();
+    return thunkAPI.rejectWithValue(error);
   }
-);
+
+  const json: { thread: Thread } = await response.json();
+
+  return json.thread;
+});
