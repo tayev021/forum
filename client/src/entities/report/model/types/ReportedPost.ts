89d3bb64e4dd60208ee93f680449b13f5a956bf9
Eugene Taranov <tayev021@gmail.com>
2026-02-01 22:09:15 +0200

    feat(client): update ReportedPost type

--- client/src/entities/report/model/types/ReportedPost.ts
+++ client/src/entities/report/model/types/ReportedPost.ts
@@ -2,7 +2,10 @@ import type { Post } from '../../../../shared/types/Post';
 import type { ReportedPostAuthor } from './ReportedPostAuthor';
 import type { ReportedPostThread } from './ReportedPostThread';
 
-export type ReportedPost = Pick<Post, 'id' | 'content' | 'createdAt'> & {
+export type ReportedPost = Pick<
+  Post,
+  'id' | 'content' | 'createdAt' | 'attachments'
+> & {
   author?: ReportedPostAuthor;
   thread: ReportedPostThread;
 };
