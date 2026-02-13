aa1cad5b41013abff12ad3536cf470f30c390aec
Eugene Taranov <tayev021@gmail.com>
2026-02-13 14:19:17 +0200

    fix(server): fix threadSchemas

--- server/src/validators/threadSchemas.ts
+++ server/src/validators/threadSchemas.ts
@@ -4,5 +4,5 @@ export const threadTitleSchema = z.object({
   title: z
     .string()
     .min(3, 'Thread title must be at least 3 characters long')
-    .max(64, 'Thread title must be a maximum of 64 characters long'),
+    .max(128, 'Thread title must be a maximum of 128 characters long'),
 });
