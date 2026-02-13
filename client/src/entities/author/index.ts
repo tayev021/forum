4cb7d7f2c49300d15c5ea04a16ed0ed9ddca257b
Eugene Taranov <tayev021@gmail.com>
2026-02-13 14:37:36 +0200

    feat(client): update author entity

--- client/src/entities/author/index.ts
+++ client/src/entities/author/index.ts
@@ -1,6 +1,18 @@
 import { authorReducer, clearAuthorError } from './model/slice/authorSlice';
 import { getAuthor } from './model/thunks/getAuthor';
+import { searchAuthors } from './model/thunks/searchAuthors';
 import { useAuthor } from './lib/hooks/useAuthor';
+import { useSearchedAuthors } from './lib/hooks/useSearchedAuthors';
 import type { Author } from './model/types/Author';
+import type { SearchedAuthor } from './model/types/SearchedAuthor';
 
-export { authorReducer, clearAuthorError, getAuthor, useAuthor, type Author };
+export {
+  authorReducer,
+  clearAuthorError,
+  getAuthor,
+  searchAuthors,
+  useAuthor,
+  useSearchedAuthors,
+  type Author,
+  type SearchedAuthor,
+};
