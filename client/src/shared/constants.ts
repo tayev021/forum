ca4a8936921ebb7a4e33739ad3a39e25deaffab9
Eugene Taranov <tayev021@gmail.com>
2026-03-31 12:16:01 +0300

    fix(client): fix constants

--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = import.meta.env.VITE_API_URL;
 export const API_URL = `${SERVER_URL}/api/v1`;
 export const POST_ATTACHMENTS_LIMIT = 8;
