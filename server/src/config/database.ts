750c9e1015d1daaf4d0d2e9cc07d04fa23643f45
Eugene Taranov <tayev021@gmail.com>
2026-03-31 12:04:18 +0300

    fix(server): fix database config

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 export const database = new Sequelize(
   process.env.DB_NAME as string,
-  process.env.DB_USERNAME as string,
+  process.env.DB_USER as string,
   process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST as string,
