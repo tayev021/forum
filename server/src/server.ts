f8d63bafa212b698fa06acb0b3ddbd368235a3bf
Eugene Taranov <tayev021@gmail.com>
2026-03-31 12:06:09 +0300

    fix(server): fix server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import { app } from './app';
 import { database } from './models';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 8080;
 
 database
   .sync()
   .then(() => {
-    app.listen(PORT, () => console.log('Server is listening'));
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
   })
   .catch((error) => {
     console.error(error);
